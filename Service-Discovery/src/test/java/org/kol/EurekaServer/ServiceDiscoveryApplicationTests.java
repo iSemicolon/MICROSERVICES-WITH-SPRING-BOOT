74c63eeb83e3c8104660db5ad3170caae5603f7b
iSemicolon <palash.samanta7497@gmail.com>
2023-01-28 10:46:25 +0530

    Service Registry [Eureka-Server] Implementation

---
+++ Service-Discovery/src/test/java/org/kol/EurekaServer/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kol.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
