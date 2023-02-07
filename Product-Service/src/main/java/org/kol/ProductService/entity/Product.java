56bec0291566b2b77bfc8e7525e40216fccd7d17
iSemicolon <palash.samanta7497@gmail.com>
2023-02-07 13:33:58 +0530

    API-gateway [ Spring-Cloud-Gateway ] Implementation

--- Product-Service/src/main/java/org/kol/ProductService/entity/Product.java
+++ Product-Service/src/main/java/org/kol/ProductService/entity/Product.java
@@ -15,7 +15,7 @@ import lombok.*;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private String name;
     private String category;
